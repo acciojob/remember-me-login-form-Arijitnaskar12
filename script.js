4729523724a22464b76d4b453a6362d62ec3f73a
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-07-02 23:57:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+let submitButton=document.getElementById("submit");
+let userName=document.getElementById("username");
+let Password=document.getElementById("password");
+let checkBox=document.getElementById("checkbox");
+let arr=JSON.parse(localStorage.getItem("userDetails"));
+let button=document.getElementById("btn");
+// console.log(arr);
+if(arr!=null)
+{
+	let existingButton=document.createElement("input");
+    existingButton.type="submit";
+    existingButton.id="existing";
+    existingButton.value="Login as a Existing user";
+    existingButton.addEventListener("click",clicked(arr));
+    button.append(existingButton);
+}
+submitButton.addEventListener("click",(event)=>{
+    // event.preventDefault();
+	let obj={
+		userName:userName.value,
+		password: Password.value
+	}
+if(checkBox.checked)
+{
+	localStorage.setItem("userDetails",JSON.stringify(obj));
+}
+alert("Logged in as"+" "+obj.userName);
+});
+function clicked(arr){
+
+    alert("Logged in as "+ arr.userName);
+}
