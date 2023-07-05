12f834deb29b498e6cf0dfff3349cea36e0cd506
Arijit Naskar <117929311+Arijitnaskar12@users.noreply.github.com>
2023-07-05 12:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let button=document.getElementById("Existing");
 // console.log(arr);
 if(arr!=null)
 {
-	button.style.display="block";
+	button.style.display="inline";
 	let data=JSON.parse(localStorage.getItem("userDetails"));
 	button.addEventListener("click",()=>{
 	alert("Logged in as"+" "+data.userName);
